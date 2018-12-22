24e44f4f6367d017df48f1b1e3caa866ff3936a8
Fernando Alegría <fernanalegria@gmail.com>
2018-12-22 21:41:23 +0100

    Get books from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css';
 import Header from './Header';
 import ListShelves from './ListShelves';
@@ -21,14 +21,17 @@ const bookShelves = [
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books: [],
     showSearchPage: false
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({
+        books
+      });
+    });
+  }
 
   render() {
     return (
@@ -61,7 +64,7 @@ class BooksApp extends Component {
         ) : (
           <div className="list-books">
             <Header />
-            <ListShelves bookShelves={bookShelves} />
+            <ListShelves bookShelves={bookShelves} books={this.state.books}/>
             <div className="open-search">
               <button onClick={() => this.setState({ showSearchPage: true })}>
                 Add a book
--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import BookShelf from './BookShelf';
 
-const ListShelves = props => (
-  <div className="list-books-content">
-    <div>
-      {props.bookShelves.map((shelf, index, array) => (
-        <BookShelf key={shelf.value} activeShelf={shelf} bookShelves={array} />
-      ))}
+const ListShelves = props => {
+  const booksByShelf = {};
+  for (let shelf of props.bookShelves) {
+    booksByShelf[shelf.value] = [];
+  }
+  for (let book of props.books) {
+    booksByShelf[book.shelf].push(book);
+  }
+
+  return (
+    <div className="list-books-content">
+      <div>
+        {props.bookShelves.map((shelf, index, array) => (
+          <BookShelf
+            key={shelf.value}
+            activeShelf={shelf}
+            bookShelves={array}
+            books={booksByShelf[shelf.value]}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ListShelves;
