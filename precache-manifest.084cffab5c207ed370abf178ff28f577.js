6d46ae21024206b5117e5ce8c1ec2f427f594f24
Fernando Alegría <fernanalegria@gmail.com>
2019-02-16 15:06:41 +0100

    Updates

---
+++ precache-manifest.084cffab5c207ed370abf178ff28f577.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7672c5ad2b42ae457ad2b79e50794b10",
+    "url": "/reactnd-project-myreads/static/media/book.7672c5ad.jpg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "c3c82f62cf18bb853171",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.c3c82f62.js"
+  },
+  {
+    "revision": "4394425e8b989da6462e",
+    "url": "/reactnd-project-myreads/static/js/main.4394425e.chunk.js"
+  },
+  {
+    "revision": "b61bfdd1df13e5906226",
+    "url": "/reactnd-project-myreads/static/js/2.b61bfdd1.chunk.js"
+  },
+  {
+    "revision": "4394425e8b989da6462e",
+    "url": "/reactnd-project-myreads/static/css/main.a351997f.chunk.css"
+  },
+  {
+    "revision": "9dc4d44a25f3b446499416ab9b32d6df",
+    "url": "/reactnd-project-myreads/index.html"
+  }
+];
